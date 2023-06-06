29f9943975a2edd92f4640f83eb2807ba480d86b
Charlon-Dale <charlondalesanagustin@gmail.com>
2023-06-06 22:56:12 +0800

    create functions

---
+++ script.js
@@ -0,0 +1,24 @@
+function getComputerChoice() {
+    
+}
+
+function playerSelection() {
+
+}
+
+function computerSelection() {
+    
+}
+
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+  }
+   
+  const playerSelection = "rock";
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
+
+  
+function game() {
+
+}  
