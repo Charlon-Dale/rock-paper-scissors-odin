b088f8c8bd447ccaa9f82ead40ae9ff62de37952
Charlon-Dale <charlondalesanagustin@gmail.com>
2023-06-19 21:30:08 +0800

    creating function for getComputerChoice

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
-function getComputerChoice() {
-    
+function getComputerChoice(computerCount) {
+  Math.floor(Math.random() * max) + 1;
+  
+  let computerCount = 0;
+  let chooseRock = "Rock";
+  let choosePaper = "Paper";
+  let chooseScissors = "Scissors";
+
+  if (computerCount == 1) {
+    return chooseRock;
+  }
+
+  else if (computerCount == 2) {
+    return choosePaper;
+  }
+
+  else if (computerCount == 3) {
+    return chooseScissors;
+  }
+  
 }
 
 function playerSelection() {
 
 }
 
-function computerSelection() {
-    
-}
-
 function playRound(playerSelection, computerSelection) {
     // your code here!
   }
    
-  const playerSelection = "rock";
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
+  // const playerSelection = "rock";
+  // const computerSelection = getComputerChoice();
+  // console.log(playRound(playerSelection, computerSelection));
 
   
 function game() {
 
 }  
+
+console.log(getComputerChoice());
